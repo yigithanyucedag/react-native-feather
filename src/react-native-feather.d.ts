95a478afd5f7913a8cc9a3f8533587ae14a16b2a
Vytenis Kuciauskas <projektas@gmail.com>
2021-06-22 23:07:17 +0300

    Add typings generator

---
+++ src/react-native-feather.d.ts
@@ -0,0 +1,569 @@
+
+export declare function Activity(props: any): any;
+
+export declare function Airplay(props: any): any;
+
+export declare function AlertCircle(props: any): any;
+
+export declare function AlertOctagon(props: any): any;
+
+export declare function AlertTriangle(props: any): any;
+
+export declare function AlignCenter(props: any): any;
+
+export declare function AlignJustify(props: any): any;
+
+export declare function AlignLeft(props: any): any;
+
+export declare function AlignRight(props: any): any;
+
+export declare function Anchor(props: any): any;
+
+export declare function Aperture(props: any): any;
+
+export declare function Archive(props: any): any;
+
+export declare function ArrowDown(props: any): any;
+
+export declare function ArrowDownCircle(props: any): any;
+
+export declare function ArrowDownLeft(props: any): any;
+
+export declare function ArrowDownRight(props: any): any;
+
+export declare function ArrowLeft(props: any): any;
+
+export declare function ArrowLeftCircle(props: any): any;
+
+export declare function ArrowRight(props: any): any;
+
+export declare function ArrowRightCircle(props: any): any;
+
+export declare function ArrowUp(props: any): any;
+
+export declare function ArrowUpCircle(props: any): any;
+
+export declare function ArrowUpLeft(props: any): any;
+
+export declare function ArrowUpRight(props: any): any;
+
+export declare function AtSign(props: any): any;
+
+export declare function Award(props: any): any;
+
+export declare function BarChart(props: any): any;
+
+export declare function BarChart2(props: any): any;
+
+export declare function Battery(props: any): any;
+
+export declare function BatteryCharging(props: any): any;
+
+export declare function Bell(props: any): any;
+
+export declare function BellOff(props: any): any;
+
+export declare function Bluetooth(props: any): any;
+
+export declare function Bold(props: any): any;
+
+export declare function Book(props: any): any;
+
+export declare function Bookmark(props: any): any;
+
+export declare function BookOpen(props: any): any;
+
+export declare function Box(props: any): any;
+
+export declare function Briefcase(props: any): any;
+
+export declare function Calendar(props: any): any;
+
+export declare function Camera(props: any): any;
+
+export declare function CameraOff(props: any): any;
+
+export declare function Cast(props: any): any;
+
+export declare function Check(props: any): any;
+
+export declare function CheckCircle(props: any): any;
+
+export declare function CheckSquare(props: any): any;
+
+export declare function ChevronDown(props: any): any;
+
+export declare function ChevronLeft(props: any): any;
+
+export declare function ChevronRight(props: any): any;
+
+export declare function ChevronsDown(props: any): any;
+
+export declare function ChevronsLeft(props: any): any;
+
+export declare function ChevronsRight(props: any): any;
+
+export declare function ChevronsUp(props: any): any;
+
+export declare function ChevronUp(props: any): any;
+
+export declare function Chrome(props: any): any;
+
+export declare function Circle(props: any): any;
+
+declare function Clipboard_2(props: any): any;
+export { Clipboard_2 as Clipboard }
+
+export declare function Clock(props: any): any;
+
+export declare function Cloud(props: any): any;
+
+export declare function CloudDrizzle(props: any): any;
+
+export declare function CloudLightning(props: any): any;
+
+export declare function CloudOff(props: any): any;
+
+export declare function CloudRain(props: any): any;
+
+export declare function CloudSnow(props: any): any;
+
+export declare function Code(props: any): any;
+
+export declare function Codepen(props: any): any;
+
+export declare function Codesandbox(props: any): any;
+
+export declare function Coffee(props: any): any;
+
+export declare function Columns(props: any): any;
+
+export declare function Command(props: any): any;
+
+export declare function Compass(props: any): any;
+
+export declare function Copy(props: any): any;
+
+export declare function CornerDownLeft(props: any): any;
+
+export declare function CornerDownRight(props: any): any;
+
+export declare function CornerLeftDown(props: any): any;
+
+export declare function CornerLeftUp(props: any): any;
+
+export declare function CornerRightDown(props: any): any;
+
+export declare function CornerRightUp(props: any): any;
+
+export declare function CornerUpLeft(props: any): any;
+
+export declare function CornerUpRight(props: any): any;
+
+export declare function Cpu(props: any): any;
+
+export declare function CreditCard(props: any): any;
+
+export declare function Crop(props: any): any;
+
+export declare function Crosshair(props: any): any;
+
+export declare function Database(props: any): any;
+
+export declare function Delete(props: any): any;
+
+export declare function Disc(props: any): any;
+
+export declare function DollarSign(props: any): any;
+
+export declare function Download(props: any): any;
+
+export declare function DownloadCloud(props: any): any;
+
+export declare function Droplet(props: any): any;
+
+export declare function Edit(props: any): any;
+
+export declare function Edit2(props: any): any;
+
+export declare function Edit3(props: any): any;
+
+export declare function ExternalLink(props: any): any;
+
+export declare function Eye(props: any): any;
+
+export declare function EyeOff(props: any): any;
+
+export declare function Facebook(props: any): any;
+
+export declare function FastForward(props: any): any;
+
+export declare function Feather(props: any): any;
+
+export declare function Figma(props: any): any;
+
+declare function File_2(props: any): any;
+export { File_2 as File }
+
+export declare function FileMinus(props: any): any;
+
+export declare function FilePlus(props: any): any;
+
+export declare function FileText(props: any): any;
+
+export declare function Film(props: any): any;
+
+export declare function Filter(props: any): any;
+
+export declare function Flag(props: any): any;
+
+export declare function Folder(props: any): any;
+
+export declare function FolderMinus(props: any): any;
+
+export declare function FolderPlus(props: any): any;
+
+export declare function Framer(props: any): any;
+
+export declare function Frown(props: any): any;
+
+export declare function Gift(props: any): any;
+
+export declare function GitBranch(props: any): any;
+
+export declare function GitCommit(props: any): any;
+
+export declare function Github(props: any): any;
+
+export declare function Gitlab(props: any): any;
+
+export declare function GitMerge(props: any): any;
+
+export declare function GitPullRequest(props: any): any;
+
+export declare function Globe(props: any): any;
+
+export declare function Grid(props: any): any;
+
+export declare function HardDrive(props: any): any;
+
+export declare function Hash(props: any): any;
+
+export declare function Headphones(props: any): any;
+
+export declare function Heart(props: any): any;
+
+export declare function HelpCircle(props: any): any;
+
+export declare function Hexagon(props: any): any;
+
+export declare function Home(props: any): any;
+
+declare function Image_2(props: any): any;
+export { Image_2 as Image }
+
+export declare function Inbox(props: any): any;
+
+export declare function Info(props: any): any;
+
+export declare function Instagram(props: any): any;
+
+export declare function Italic(props: any): any;
+
+export declare function Key(props: any): any;
+
+export declare function Layers(props: any): any;
+
+export declare function Layout(props: any): any;
+
+export declare function LifeBuoy(props: any): any;
+
+export declare function Link(props: any): any;
+
+export declare function Link2(props: any): any;
+
+export declare function Linkedin(props: any): any;
+
+export declare function List(props: any): any;
+
+export declare function Loader(props: any): any;
+
+export declare function Lock(props: any): any;
+
+export declare function LogIn(props: any): any;
+
+export declare function LogOut(props: any): any;
+
+export declare function Mail(props: any): any;
+
+export declare function Map(props: any): any;
+
+export declare function MapPin(props: any): any;
+
+export declare function Maximize(props: any): any;
+
+export declare function Maximize2(props: any): any;
+
+export declare function Meh(props: any): any;
+
+export declare function Menu(props: any): any;
+
+export declare function MessageCircle(props: any): any;
+
+export declare function MessageSquare(props: any): any;
+
+export declare function Mic(props: any): any;
+
+export declare function MicOff(props: any): any;
+
+export declare function Minimize(props: any): any;
+
+export declare function Minimize2(props: any): any;
+
+export declare function Minus(props: any): any;
+
+export declare function MinusCircle(props: any): any;
+
+export declare function MinusSquare(props: any): any;
+
+export declare function Monitor(props: any): any;
+
+export declare function Moon(props: any): any;
+
+export declare function MoreHorizontal(props: any): any;
+
+export declare function MoreVertical(props: any): any;
+
+export declare function MousePointer(props: any): any;
+
+export declare function Move(props: any): any;
+
+export declare function Music(props: any): any;
+
+export declare function Navigation(props: any): any;
+
+export declare function Navigation2(props: any): any;
+
+export declare function Octagon(props: any): any;
+
+export declare function Package(props: any): any;
+
+export declare function Paperclip(props: any): any;
+
+export declare function Pause(props: any): any;
+
+export declare function PauseCircle(props: any): any;
+
+export declare function PenTool(props: any): any;
+
+export declare function Percent(props: any): any;
+
+export declare function Phone(props: any): any;
+
+export declare function PhoneCall(props: any): any;
+
+export declare function PhoneForwarded(props: any): any;
+
+export declare function PhoneIncoming(props: any): any;
+
+export declare function PhoneMissed(props: any): any;
+
+export declare function PhoneOff(props: any): any;
+
+export declare function PhoneOutgoing(props: any): any;
+
+export declare function PieChart(props: any): any;
+
+export declare function Play(props: any): any;
+
+export declare function PlayCircle(props: any): any;
+
+export declare function Plus(props: any): any;
+
+export declare function PlusCircle(props: any): any;
+
+export declare function PlusSquare(props: any): any;
+
+export declare function Pocket(props: any): any;
+
+export declare function Power(props: any): any;
+
+export declare function Printer(props: any): any;
+
+export declare function Radio(props: any): any;
+
+export declare function RefreshCcw(props: any): any;
+
+export declare function RefreshCw(props: any): any;
+
+export declare function Repeat(props: any): any;
+
+export declare function Rewind(props: any): any;
+
+export declare function RotateCcw(props: any): any;
+
+export declare function RotateCw(props: any): any;
+
+export declare function Rss(props: any): any;
+
+export declare function Save(props: any): any;
+
+export declare function Scissors(props: any): any;
+
+export declare function Search(props: any): any;
+
+export declare function Send(props: any): any;
+
+export declare function Server(props: any): any;
+
+export declare function Settings(props: any): any;
+
+export declare function Share(props: any): any;
+
+export declare function Share2(props: any): any;
+
+export declare function Shield(props: any): any;
+
+export declare function ShieldOff(props: any): any;
+
+export declare function ShoppingBag(props: any): any;
+
+export declare function ShoppingCart(props: any): any;
+
+export declare function Shuffle(props: any): any;
+
+export declare function Sidebar(props: any): any;
+
+export declare function SkipBack(props: any): any;
+
+export declare function SkipForward(props: any): any;
+
+export declare function Slack(props: any): any;
+
+export declare function Slash(props: any): any;
+
+export declare function Sliders(props: any): any;
+
+export declare function Smartphone(props: any): any;
+
+export declare function Smile(props: any): any;
+
+export declare function Speaker(props: any): any;
+
+export declare function Square(props: any): any;
+
+export declare function Star(props: any): any;
+
+export declare function StopCircle(props: any): any;
+
+export declare function Sun(props: any): any;
+
+export declare function Sunrise(props: any): any;
+
+export declare function Sunset(props: any): any;
+
+export declare function Tablet(props: any): any;
+
+export declare function Tag(props: any): any;
+
+export declare function Target(props: any): any;
+
+export declare function Terminal(props: any): any;
+
+export declare function Thermometer(props: any): any;
+
+export declare function ThumbsDown(props: any): any;
+
+export declare function ThumbsUp(props: any): any;
+
+export declare function ToggleLeft(props: any): any;
+
+export declare function ToggleRight(props: any): any;
+
+export declare function Tool(props: any): any;
+
+export declare function Trash(props: any): any;
+
+export declare function Trash2(props: any): any;
+
+export declare function Trello(props: any): any;
+
+export declare function TrendingDown(props: any): any;
+
+export declare function TrendingUp(props: any): any;
+
+export declare function Triangle(props: any): any;
+
+export declare function Truck(props: any): any;
+
+export declare function Tv(props: any): any;
+
+export declare function Twitch(props: any): any;
+
+export declare function Twitter(props: any): any;
+
+export declare function Type(props: any): any;
+
+export declare function Umbrella(props: any): any;
+
+export declare function Underline(props: any): any;
+
+export declare function Unlock(props: any): any;
+
+export declare function Upload(props: any): any;
+
+export declare function UploadCloud(props: any): any;
+
+export declare function User(props: any): any;
+
+export declare function UserCheck(props: any): any;
+
+export declare function UserMinus(props: any): any;
+
+export declare function UserPlus(props: any): any;
+
+export declare function Users(props: any): any;
+
+export declare function UserX(props: any): any;
+
+export declare function Video(props: any): any;
+
+export declare function VideoOff(props: any): any;
+
+export declare function Voicemail(props: any): any;
+
+export declare function Volume(props: any): any;
+
+export declare function Volume1(props: any): any;
+
+export declare function Volume2(props: any): any;
+
+export declare function VolumeX(props: any): any;
+
+export declare function Watch(props: any): any;
+
+export declare function Wifi(props: any): any;
+
+export declare function WifiOff(props: any): any;
+
+export declare function Wind(props: any): any;
+
+export declare function X(props: any): any;
+
+export declare function XCircle(props: any): any;
+
+export declare function XOctagon(props: any): any;
+
+export declare function XSquare(props: any): any;
+
+export declare function Youtube(props: any): any;
+
+export declare function Zap(props: any): any;
+
+export declare function ZapOff(props: any): any;
+
+export declare function ZoomIn(props: any): any;
+
+export declare function ZoomOut(props: any): any;
+
+export { }
